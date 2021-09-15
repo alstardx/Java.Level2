cb63357bb2fa897b8ea24e4d6b354c2c0fedf4fe
aleks <alexander_at@inbox.lv>
2021-09-15 11:23:11 +0100

    Homework2

--- Homework2/MyArrayDataException.java
+++
@@ -1,7 +0,0 @@
-package Lesson002;
-
-public class MyArrayDataException extends RuntimeException {
-    public MyArrayDataException(String message) {
-        super("Некорректные данные "+message);
-    }
-}
